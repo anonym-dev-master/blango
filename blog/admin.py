4df4cbc994cdac9c9e74228779cc70a3234450bb
unknown <aaaqibarsaduet@gmail.com>
2025-07-17 01:36:42 +0500

    added the reverse generic relation for models

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Tag, Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("title",)}
+  list_display = ("title", "slug", "published_at")
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Comment)
